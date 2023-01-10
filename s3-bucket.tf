33cf4bf4cee91c7c77fa10ccef5d0a238d250c39
steve <smuir623@gmail.com>
2023-01-10 19:43:26 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "steve-muir"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/SMCorp/s3-bucket/aws"
+  version = "2.8.0"
 }
