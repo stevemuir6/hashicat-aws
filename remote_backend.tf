f8ddad7cf32de5662fece449db176a4bd283a5d9
stevemuir6 <68389769+stevemuir6@users.noreply.github.com>
2023-01-11 11:29:39 -0500

    Update remote_backend.tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
@@ -8,14 +8,4 @@ terraform {
     }
   }
 }
-=======
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "SMCorp"
-    workspaces {
-      name = "hashicat-aws"
-    }
-  }
-}
->>>>>>> 33cf4bf4cee91c7c77fa10ccef5d0a238d250c39
+
